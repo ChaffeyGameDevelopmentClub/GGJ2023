3cdfc77270e9a67aa5be7c04afcf64e1c331f3ce
Kamran Haq <KamranHaq@protonmail.com>
2023-01-30 15:21:55 -0800

    Simple movement controller with jump

--- Assets/scripts/Player.gd
+++ Assets/scripts/Player.gd
@@ -6,11 +6,10 @@ class_name Player
 # Declare member variables here. 
 const G := Vector2(0, 9.8)
 export var speed := 300.0
+export var jump_vector := Vector2(0, -300)
 var _velocity := Vector2.ZERO
 var _input_vector = Vector2.ZERO
-
 var seed_planter : SeedPlanter
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,11 +18,9 @@ func _ready():
 	var model = PlatformTreeSeed.new()
 	seed_planter.set_seed_model(model)
 
-
 # Called when the player is killed.	
 func _on_player_killed() -> void:
 	print("Game OVER!")
-
 
 #handle input events
 func _input(event):
@@ -32,13 +29,16 @@ func _input(event):
 			seed_planter.plant_seed()
 			#lower_health(10)
 
-func _process(delta):
+func _process(_delta):
 	_input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	_input_vector = _input_vector.normalized()
 
+	if Input.is_action_just_pressed("ui_select") and is_on_floor():
+		_velocity += jump_vector
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
+func _physics_process(_delta):
 	_velocity.x = 0
+	_velocity.x += _input_vector.x*speed
 	_velocity += G
-	_velocity += _input_vector*speed
-	move_and_slide(_velocity, Vector2.UP)
+	_velocity = move_and_slide(_velocity, Vector2.UP)
