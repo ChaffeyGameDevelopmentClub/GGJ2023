9a9638e4824e30cd5bda4ae96f63dca513c7630f
Thomas Margraff <thomaspmar@protonmail.com>
2023-01-30 07:32:29 -0800

    Finished ClampedInteger. Switched LivingObject.Health to ClampedInteger

--- Assets/scripts/ClampedInteger.gd
+++ Assets/scripts/ClampedInteger.gd
@@ -1,52 +1,65 @@
+# Represents an integer that is clamped between a minimum and maximum range.
+
 extends Object
 class_name ClampedInteger
 
+# The maximum value this integer can be. Do not set this directly, use set_max_value() instead.
 var _max_value := 0
+
+# The lowest value this integer can be. Do not set this directly, use set_min_value() instead.
 var _min_value := 0
+
+# The current value of this integer. Do not set this directly, use set_value() instead.
 var _value := 0
 
 # Called whenever the current value changes.
 signal on_value_changed(old_value, new_value)
+
 
 func _init(min_value, max_value):
 	set_min_value(min_value)
 	set_max_value(max_value)
 
 
-# Raise the current value.
+# Returns the current value.
+func get_value() -> int:
+	return _value
+
+# Raise the current value. Will be clamped between the minimum and maximum value.
 func raise_value(amount : int) -> void:
 	set_value(_value + amount)
 
-# Lower the current value.
+# Lower the current value. Will be clamped between the minimum and maximum value.
 func lower_value(amount : int) -> void:
 	set_value(_value - amount)
 
-# Set the current value.
+# Set the current value. Will be clamped between the minimum and maximum value.
 func set_value(new_value : int) -> void:
-	pass
+	var old_value = _value
+	_value = clamp(new_value, _min_value, _max_value)
+
+	if _value != old_value:
+		emit_signal("on_value_changed", old_value, _value)
 
 
-# Raise the max value.
-func raise_max_value(amount : int) -> void:
-	set_max_value(_max_value + amount)
-
-# Lower the max value.
-func lower_max_value(amount : int) -> void:
-	set_max_value(_max_value - amount)
-
-# Set the max value.
+# Set the max value. Will lower current value if the new max value is lower than the current value.
 func set_max_value(new_value : int) -> void:
-	pass
+	if new_value < _min_value:
+		printerr("Warning, ClampedInteger can't set the max value to lower than the minimum value.")
+		return
 
+	_max_value = new_value
 
-# Raise the min value.
-func raise_min_value(amount : int) -> void:
-	set_min_value(_min_value + amount)
+	if _value > _max_value:
+		set_value(_max_value)
 
-# Lower the min value.
-func lower_min_value(amount : int) -> void:
-	set_min_value(_min_value - amount)
-
-# Set the min value.
+# Set the min value. Will raise current value if the new min value is higher than the current value.
 func set_min_value(new_value : int) -> void:
-	pass
+	if new_value > _max_value:
+		printerr("Warning, ClampedInteger can't set the minimum value higher than the maximum value.")
+		return
+
+	_min_value = new_value
+
+	if _value < _min_value:
+		set_value(_min_value)
--- Assets/scripts/LivingObject.gd
+++ Assets/scripts/LivingObject.gd
@@ -2,72 +2,67 @@ extends KinematicBody2D
 
 class_name LivingObject
 
-# The maximum value for _health.
+# The maximum value for health.
 export var max_health := 100
 
-# Can the _health exceed max _health, for example if the player drinks a _health potion?
+# Can the health exceed max health, for example if the player drinks a health potion?
 export var can_exceed_max_health := false
 
-# switch this to a ClampedInteger
-var _health := 0
+var health : ClampedInteger
 var _has_died := false
 var _is_initialized := false
 
-# Called when this object runs out of _health and is killed.
+# Called when this object runs out of health and is killed.
 signal on_killed()
-
-# Called whenever the _health changes for this object.
-signal on_health_changed(old_health, new_health)
 
 
 func _ready():
-    set_health(max_health)
-    _is_initialized = true
+	health = ClampedInteger.new(0, max_health)
+	set_health(max_health)
+	_is_initialized = true
 
 
-# Returns whether or not this object has run out of _health and has died.
+# Returns whether or not this object has run out of health and has died.
 func is_dead() -> bool:
-    return _has_died or _health <= 0
+	return _has_died or health.get_value() <= 0
 
 # Returns the current health of this object.
 func get_health() -> int:
-    return _health
+	return health.get_value()
 
-# Raise _health by an amount. 
-# Returns the new _health for convenience.
+# Raise health by an amount. 
+# Returns the new health for convenience.
 func raise_health(amount : int) -> int:
-    return set_health(_health + amount)
+	return set_health(health.get_value() + amount)
 
-# Lowers _health by an amount. 
+# Lowers health by an amount. 
 # Returns the new health for convenience.
 func lower_health(amount : int) -> int:
-    return set_health(_health - amount)
+	return set_health(health.get_value() - amount)
 
 # Set the health to a new value. 
 # Returns the new health for convenience.
 func set_health(new_health : int) -> int:
-    
-    # exit function if we're already dead.
-    if _is_initialized and is_dead():
-        return _health
-        
-    # make sure data is valid.
-    if new_health < 0:
-        new_health = 0
-    elif not can_exceed_max_health and new_health > max_health:
-        new_health = max_health
+	
+	# Exit function if we're already dead.
+	if _is_initialized and is_dead():
+		return health.get_value()
 
-    # change _health and fire signal.
-    var old_health = _health
-    _health = new_health
-    emit_signal("on_health_changed", old_health, _health)
-    print("Health for \"%s\" has been set to %s" % [name, _health])
+	# Change health and fire signal.
+	var old_health = health.get_value()
+	health.set_value(new_health)
 
-    # check if dead.
-    if is_dead():
-        _has_died = true
-        print("\"%s\" has died" % name)
-        emit_signal("on_killed")        
+	# Exit function if no change was made.
+	if health.get_value() == old_health:
+		return health.get_value()
 
-    return _health
+	print("Health for \"%s\" has been set to %s" % [name, health.get_value()])
+
+	# Check if dead.
+	if is_dead():
+		_has_died = true
+		print("\"%s\" has died" % name)
+		emit_signal("on_killed")        
+
+	return health.get_value()
 
