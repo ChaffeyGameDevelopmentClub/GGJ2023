b92ec92d811cb8ed604c6127b68c4a85ed755fd5
David Reyes <96704998+floatingbridgeofdreams@users.noreply.github.com>
2023-01-30 23:23:49 -0800

    new jump physics(not working)
    new jump that goes higher the longer space is pressed

--- Assets/scripts/Player.gd
+++ Assets/scripts/Player.gd
@@ -3,8 +3,11 @@
 extends LivingObject
 
 # Declare member variables here. 
-const G := Vector2(0, 9.8)
+export var gravity = 8
 export var speed := 300.0
+export var fallMultiplier := 2
+export var lowJumpMultiplier := 10
+export var jumpVelocity := 400
 export var jump_vector := Vector2(0, -300)
 var _velocity := Vector2.ZERO
 var _input_vector = Vector2.ZERO
@@ -14,6 +17,7 @@ var can_plant_seed := false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+# warning-ignore:return_value_discarded
 	connect("on_killed", self, "_on_player_killed")
 	seed_planter = get_node("SeedPlanter")
 	var model = PlatformTreeSeed.new()
@@ -33,16 +37,19 @@ func _input(event):
 func _process(_delta):
 	_input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	_input_vector = _input_vector.normalized()
-
-	if Input.is_action_just_pressed("ui_select") and is_on_floor():
-		_velocity += jump_vector
-
+	_velocity.y += gravity
+	if _velocity.y > 0:
+		_velocity +=  Vector2.UP * (fallMultiplier) * (-9.81)  
+	elif Input.is_action_just_released("ui_select") and _velocity.y < 0:
+		_velocity += Vector2.UP * (-9.81) * (lowJumpMultiplier)
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_accept"):
+			_velocity = Vector2.UP * jumpVelocity
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta):
 	_velocity.x = 0
 	_velocity.x += _input_vector.x*speed
-	_velocity += G
-	_velocity = move_and_slide(_velocity, Vector2.UP)
+	_velocity = move_and_slide(_velocity, Vector2(0,-1))
 
 	last_collision = get_last_slide_collision()
 	
