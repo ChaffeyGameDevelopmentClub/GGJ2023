6277e6a6178811f37c81ec234763d0dba9ebfff6
Kamran Haq <KamranHaq@protonmail.com>
2023-02-03 18:03:47 -0800

    Seed cycling
    Press E to cycle between seeds

--- Assets/scripts/BouncyFlower.gd
+++ Assets/scripts/BouncyFlower.gd
@@ -9,6 +9,9 @@ export var BouncyPower = 1.5
 
 #When a body enters the hit box of the flower then the body will get shoot up
 func _on_HitBoxs_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
-	print(body)
-	body._velocity =  Vector2(0, 0)
-	body._velocity += body.jump_vector * BouncyPower
+	if body == Player:
+		#body._velocity = Vector2(0, 0)
+		if body._velocity.y > 0:
+			body._velocity.y *= -0.6
+			#body._velocity.y += 1
+		#body._velocity.y += 
